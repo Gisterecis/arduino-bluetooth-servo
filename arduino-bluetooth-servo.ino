d974751cd30b58f77c3250e996926db003132781
Vladislav Alexandrovich <81512332+Gisterecis@users.noreply.github.com>
2021-04-23 14:04:32 +0300

    Add files via upload
    Main script.

---
+++ arduino-bluetooth-servo.ino
@@ -0,0 +1,17 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+SoftwareSerial bluetooth(0, 1);
+Servo servo;
+int data;
+void setup()
+{
+  bluetooth.begin(9600);
+  pinMode(9, OUTPUT);
+  servo.attach(9);
+}
+void loop()
+{
+  data = bluetooth.read();
+  servo.write(data);
+  delay(1000);
+}
